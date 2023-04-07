0499db4ba4e935448e26213f41b61ee5bc7bd563
Bordiuzhan <bordyujan@gmail.com>
2023-04-07 14:06:34 +0200

    fix

--- src/redux/recipes/operations.js
+++ src/redux/recipes/operations.js
@@ -1,14 +1,13 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL =
-  'https://soyummy-qk5m.onrender.com/api/ingredients/list';
+axios.defaults.baseURL = 'https://soyummy-qk5m.onrender.com/api';
 
 export const fetchIngredients = createAsyncThunk(
   'ingredients/fetchAll',
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get('/ingredients');
+      const response = await axios.get('recipes/ingredients/list');
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
