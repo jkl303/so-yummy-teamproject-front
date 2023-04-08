0d1306d68b8a3e3a3a179afa87cdaf9da24f3f04
Makarov Yelisei <fenniz123@gmail.com>
2023-04-08 15:01:52 +0300

    NotFound

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -1,5 +1,23 @@
-// import {} from './NotFound.styled';
+import {
+  First,
+  Second,
+  Third,
+  Title,
+  Descr,
+  Section,
+  Img,
+} from './NotFoundPage.styled';
+import IMG from '../../images/desktop/404-page-notfound1440-2x.png';
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <Section>
+      <First></First>
+      <Second></Second>
+      <Third></Third>
+      <Img src={IMG} alt="" />
+      <Title>We are sorry,</Title>
+      <Descr>but the page you were looking for can’t be found..</Descr>
+    </Section>
+  );
 }
---
+++ src/pages/NotFoundPage/NotFoundPage.styled.js
@@ -0,0 +1,150 @@
+import styled from 'styled-components';
+
+export const Section = styled.div`
+  position: relative;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 375px;
+  padding-top: 220px;
+
+  @media screen and (min-width: 768px) {
+    max-width: 768px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    max-width: 1440px;
+  }
+`;
+
+export const Title = styled.h1`
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 24px;
+  font-size: 18px;
+  line-height: 20px;
+  letter-spacing: -0.02em;
+  color: #000000;
+
+  margin-top: 14px;
+  margin-bottom: 8px;
+  text-align: center;
+
+  @media screen and (min-width: 768px) {
+    font-size: 24px;
+    line-height: 24px;
+    margin-top: 32px;
+    margin-bottom: 14px;
+  }
+`;
+
+export const First = styled.div`
+  position: absolute;
+
+  width: 8px;
+  height: 8px;
+  left: 120px;
+  top: 79.38px;
+
+  background: #8baa36;
+  border-radius: 3px;
+  transform: rotate(-25deg);
+
+  @media screen and (min-width: 768px) {
+    left: 219px;
+    top: 90.92px;
+    width: 14px;
+    height: 14px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    left: 328px;
+    top: 121.92px;
+  }
+`;
+
+export const Second = styled.div`
+  position: absolute;
+  width: 6px;
+  height: 6px;
+  left: 247px;
+  top: 144px;
+
+  background: #22252a;
+  border-radius: 3px;
+  transform: rotate(-25deg);
+
+  @media screen and (min-width: 768px) {
+    width: 12px;
+    height: 12px;
+    left: 437px;
+    top: 160.07px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    left: 807px;
+    top: 204.07px;
+  }
+`;
+
+export const Third = styled.div`
+  position: absolute;
+  width: 8px;
+  height: 8px;
+  left: 349px;
+  top: 111.85px;
+
+  background: #8baa36;
+  border-radius: 3px;
+  transform: rotate(-25deg);
+
+  @media screen and (min-width: 768px) {
+    width: 14px;
+    height: 14px;
+    left: 726px;
+    top: 103.92px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    left: 1250px;
+    top: 134.92px;
+  }
+`;
+
+export const Descr = styled.p`
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 400;
+
+  font-size: 14px;
+  line-height: 18px;
+  letter-spacing: -0.02em;
+  font-feature-settings: 'liga' off;
+  text-align: center;
+  color: #000000;
+  opacity: 0.5;
+
+  @media screen and (min-width: 768px) {
+    font-size: 18px;
+    line-height: 24px;
+  }
+`;
+
+export const Img = styled.img`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 259px;
+  max-height: 170px;
+
+  @media screen and (min-width: 768px) {
+    max-width: 498px;
+    max-height: 327px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    max-width: 498px;
+    max-height: 331px;
+  }
+`;
