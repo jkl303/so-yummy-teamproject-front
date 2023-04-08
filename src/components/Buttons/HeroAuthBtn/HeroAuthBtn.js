556913100ac5eab6fca68a33bbe2c9b02cf05503
Daria-Zakharova <dariazakharova.smthelse@gmail.com>
2023-04-08 21:58:30 +0300

    Add some btns, refactor to links, rename, add text prop

--- src/components/Buttons/HeroAuthBtn/HeroAuthBtn.js
+++ src/components/Buttons/HeroAuthBtn/HeroAuthBtn.js
@@ -1,17 +1,17 @@
 import { CustomBtn } from 'components/Buttons/templates/CustomBtn/CustomBtn.styled';
 
-export const HeroAuthBtn = ({ active, children }) => {
+export const HeroAuthBtn = ({ active, text }) => {
   const sizes = {
     m: { w: 95, h: 45, padH: 24, padV: 12 },
     t: { w: 141, h: 67, padH: 44, padV: 22 },
   };
   return active ? (
-    <CustomBtn as="a" variant="changeBgInvert" sizes={sizes}>
-      {children}
+    <CustomBtn variant="changeBgInvert" sizes={sizes}>
+      {text}
     </CustomBtn>
   ) : (
-    <CustomBtn as="a" variant="changeOutline" sizes={sizes}>
-      {children}
+    <CustomBtn variant="changeOutline" sizes={sizes}>
+      {text}
     </CustomBtn>
   );
 };
