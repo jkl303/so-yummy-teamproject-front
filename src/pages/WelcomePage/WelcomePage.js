08f1cb01c3eeae27a6fd44d5a0e973db30e98e91
Sergey-Ryabko-84 <amigoswow@ukr.net>
2023-04-10 02:41:15 +0300

    adds WelcomPage

--- src/pages/WelcomePage/WelcomePage.js
+++ src/pages/WelcomePage/WelcomePage.js
@@ -1,5 +1,25 @@
-import {} from './WelcomePage.styled';
+import { NavLink } from 'react-router-dom';
+import { Logo, Title, Text, WelcomWrapper, Link } from './WelcomePage.styled';
+import { HeroAuthBtn } from 'components/Buttons/HeroAuthBtn/HeroAuthBtn';
 
 export default function WelcomePage() {
-  return <div>Welcome</div>;
+  return (
+    <WelcomWrapper>
+      <Logo />
+      <Title>Welcome to the app!</Title>
+      <Text>
+        This app offers more than just a collection of recipes - it is designed
+        to be your very own digital cookbook. You can easily save and retrieve
+        your own recipes at any time.
+      </Text>
+      <div>
+        <NavLink to="/register">
+          <HeroAuthBtn active text="Registration" />
+        </NavLink>
+        <NavLink to="/login">
+          <HeroAuthBtn text="Sign In" />
+        </NavLink>
+      </div>
+    </WelcomWrapper>
+  );
 }
--- src/pages/WelcomePage/WelcomePage.styled.js
+++ src/pages/WelcomePage/WelcomePage.styled.js
@@ -1,1 +1,72 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
+import useWindowDimensions from 'hooks/useWindowDimensions';
+import { ReactComponent as SvgLogo } from 'images/svg/logo.svg';
+import bgImg375 from 'images/mobile/start-bg375.jpg';
+import bgImg768 from 'images/tablet/start-bg768.jpg';
+import bgImg1440 from 'images/desktop/start-bg1440.jpg';
+
+export const WelcomWrapper = styled.div`
+  width: 100%;
+  height: ${() => {
+    const { height } = useWindowDimensions();
+    return `${height}px`;
+  }};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 274px;
+  background-image: url(${bgImg375});
+  background-size: cover;
+  color: #fafafa;
+
+  @media screen and (min-width: 768px) {
+    padding-top: 342px;
+    background-image: url(${bgImg768});
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding-top: 214px;
+    background-image: url(${bgImg1440});
+  }
+`;
+
+export const Logo = styled(SvgLogo)`
+  width: 54px;
+  height: 54px;
+  margin-bottom: 28px;
+
+  @media screen and (min-width: 768px) {
+    width: 68px;
+    height: 68px;
+  }
+`;
+
+export const Title = styled.h1`
+  margin-bottom: 14px;
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 1;
+
+  @media screen and (min-width: 768px) {
+    font-size: 28px;
+  }
+`;
+
+export const Text = styled.p`
+  display: inline-block;
+  width: 304px;
+  margin-bottom: 44px;
+  text-align: center;
+  font-size: 14px;
+  line-height: 1.29;
+
+  @media screen and (min-width: 768px) {
+    width: 492px; //500
+    margin-bottom: 40px;
+    font-size: 18px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 532px; //540
+  }
+`;
