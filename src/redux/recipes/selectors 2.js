da7922737669aabe9312a69a6ce81ebd4e1840ec
alexanderyar <103504393+alexanderyar@users.noreply.github.com>
2023-04-11 14:56:53 -0600

    added .env

--- src/redux/recipes/selectors 2.js
+++
@@ -1,6 +0,0 @@
-export const selectFilter = state => state.filter;
-// export const selectContacts = state => state...;
-// export const selectIsLoading = state => state...;
-// export const selectError = state => state...;
-
-//можете названия менять на какие угодно
