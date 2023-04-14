e92f4dafb78fed762ac953a995b3ba9625d37ca1
Andrew Burlachenko <ap.burla4enko@gmail.com>
2023-04-14 16:11:36 +0300

    added some styles

--- src/components/AddRecipePage/FollowUs/FollowUs.js
+++ src/components/AddRecipePage/FollowUs/FollowUs.js
@@ -2,9 +2,9 @@ import { SocialList } from 'components/SocialList/SocialList';
 
 export const FollowUs = () => {
   return (
-    <>
+    <div>
       <h2>Follow us</h2>
       <SocialList />
-    </>
+    </div>
   );
 };
